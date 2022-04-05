2d157888caad2f025ed31a77923fed1723ed6faf
ptrv-dev <petrov99119@gmail.com>
2022-04-05 21:48:58 +0300

    Section FAQ done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
-console.log("JavaScript is working!")
+console.log("JavaScript is working!");
+
+const tabsButton = document.querySelectorAll(".faq-tabs__button"),
+    tabsItem = document.querySelectorAll(".faq-tabs__item");
+
+for (const [i, btn] of tabsButton.entries()) {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault(false);
+        if (tabsItem[i].classList.contains("faq-tabs__item_active"))
+            return tabsItem[i].classList.remove("faq-tabs__item_active");
+        tabsItem.forEach((element) => {
+            element.classList.remove("faq-tabs__item_active");
+        });
+        tabsItem[i].classList.add("faq-tabs__item_active");
+    });
+}
